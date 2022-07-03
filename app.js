90a905abca6f368c14edd7658857ad33fc9e499f
Florian Ipate <florian.ipate@gmail.com>
2022-07-03 18:29:12 +0100

    bug fix

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const carouselSlider = document.querySelector('.carousel-container');
+const carouselSlider = document.querySelector('.carousel-slide');
 const carouselImages = document.querySelectorAll('.carousel-slide img');
 
 //buttons
@@ -9,7 +9,7 @@ let counter = 1;
 const size = carouselImages[0].clientWidth;
 carouselSlider.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
-nextBtn.addEventListener('click', () => {
+nextBtn.addEventListener('click',() => {
     carouselSlider.style.transition = "transform 0.4s ease-in-out";
     counter++;
     // console.log(counter);
