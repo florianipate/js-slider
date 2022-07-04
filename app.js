7d8ff5d469c3a1941c549509290e4edb684a96b0
Florian Ipate <florian.ipate@gmail.com>
2022-07-04 08:39:07 +0100

    remove bug

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const carouselSlider = document.querySelector('.carousel-slide');
 const carouselImages = document.querySelectorAll('.carousel-slide img');
-const carouselSize = carouselImages.length;
-console.log(carouselImages.length);
+const carouselFullSize = carouselImages.length;
+var cont = 0;
+carouselImages.forEach((element) => {
+    if(element.style.display != 'none'){
+        cont++;
+    }
+});
+
+// if(carouselImages[0]:visible.length == 0){
+//     const carouselSize = carouselImages.length;
+// }
+
+// console.log(carouselImages[2].style.display);
+console.log(cont);
 //buttons
 const prevBtn = document.querySelector('#prevBtn');
 const nextBtn = document.querySelector('#nextBtn');
@@ -11,6 +23,7 @@ const size = carouselImages[0].clientWidth;
 carouselSlider.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
 nextBtn.addEventListener('click',() => {
+    if(counter >= cont) return;
     carouselSlider.style.transition = "transform 0.4s ease-in-out";
     counter++;
     console.log(counter);
@@ -18,6 +31,7 @@ nextBtn.addEventListener('click',() => {
 
 });
 prevBtn.addEventListener('click',() => {
+    if(counter <= -1 ) return;
     carouselSlider.style.transition = "transform 0.4s ease-in-out";
     counter--;
     // console.log(counter);
@@ -26,14 +40,14 @@ prevBtn.addEventListener('click',() => {
 });
 
 carouselSlider.addEventListener('transitionend', () => {
-    if(counter === carouselSize){
+    if(counter === cont){
         carouselSlider.style.transition = "none";
         counter = 0;
         carouselSlider.style.transform = 'translateX(' + (-size * counter) + 'px)';
     }
     if(counter === -1){
         carouselSlider.style.transition = "none";
-        counter = carouselSize-1;
+        counter = cont-1;
         carouselSlider.style.transform = 'translateX(' + (-size * counter) + 'px)';
     }
 })
